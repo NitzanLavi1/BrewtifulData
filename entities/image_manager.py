926b688fdac58dbed792c64e6946032d57f1b202
Nitzan Lavi <nitzanlavi2@gmail.com>
2025-09-14 17:18:58 -0600

    add typehing refactor functions

--- entities/image_manager.py
+++ entities/image_manager.py
@@ -1,28 +1,33 @@
 import os
 import requests
+from typing import Optional
 
 class ImageManager:
-    def __init__(self, image_url, image_dir):
+    def __init__(self, image_url: str, image_dir: str) -> None:
         self.image_url = image_url
         self.image_dir = image_dir
 
-    def has_image(self):
+    def has_image(self) -> bool:
+
         return bool(self.image_url)
 
-    def image_filename(self):
+    def image_filename(self) -> str:
+
         return self.image_url.split("/")[-1] if self.image_url else "N/A"
 
-    def download_image(self):
+    def download_image(self) -> Optional[str]:
         if not self.has_image():
+
             return None
         image_name = self.image_filename()
         image_path = os.path.join(self.image_dir, image_name)
         img_resp = requests.get(self.image_url)
         with open(image_path, "wb") as f:
             f.write(img_resp.content)
+
         return image_path
 
     @staticmethod
-    def remove_image(image_path):
+    def remove_image(image_path: str) -> None:
         if image_path and os.path.exists(image_path):
             os.remove(image_path)
--- entities/page.py
+++ entities/page.py
@@ -2,19 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 from core.config import BASE_URL, HEADERS
 from entities.beer import Beer
+from typing import List, Optional
 
 class Page:
-    def __init__(self, page_num):
-        self.page_num = page_num
-        self.url = BASE_URL + str(page_num)
-        self.soup = None
+    def __init__(self, page_num: int) -> None:
+        self.page_num: int = page_num
+        self.url: str = BASE_URL + str(page_num)
+        self.soup: Optional[BeautifulSoup] = None
 
-    def fetch(self):
+    def fetch(self) -> None:
         response = requests.get(self.url, headers=HEADERS)
         response.raise_for_status()
         self.soup = BeautifulSoup(response.text, "html.parser")
 
-    def get_beers(self):
+    def get_beers(self) -> List[Beer]:
         if self.soup is None:
             self.fetch()
         beer_rows = self.soup.select("div.beer-row")
